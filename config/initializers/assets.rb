7aa6ead14eb1a57eb092be37db17e390a8a7b44b
eileencodes <eileencodes@gmail.com>
2015-07-28 12:39:12 -0400

    Update asset precompile initializer

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -1,11 +1,13 @@
-# Be sure to restart your server when you modify this file.
+RegressionSprockets::Application.configure do
+  # Bump to expire all assets. The big hammer.
+  config.assets.version = 'v1'
 
-# Version of your assets, change this if you want to expire all your assets.
-Rails.application.config.assets.version = '1.0'
+  # One for each toplevel asset we compile.
+  config.assets.precompile += %w(
+    application.css application.js
+  )
 
-# Add additional assets to the asset load path
-# Rails.application.config.assets.paths << Emoji.images_path
-
-# Precompile additional assets.
-# application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
-# Rails.application.config.assets.precompile += %w( search.js )
+  # Serve images, CSS, and JavaScript from the CDN. Falls back to the current host.
+  ActionController::Base.asset_host = config.x.assets.cdn_host
+  ActionMailer::Base.asset_host = config.x.assets.cdn_host
+end
